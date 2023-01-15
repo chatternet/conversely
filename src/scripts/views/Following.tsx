1081010cc4036ab597eacefb0334ad6de5640431
Garrin McGoldrick <garrin.mcgoldrick@gmail.com>
2023-01-15 10:08:25 -0500

    implement unfollow

--- src/scripts/views/Following.tsx
+++ src/scripts/views/Following.tsx
@@ -1,6 +1,6 @@
 import { UseState } from "../commonutils";
 import { FormatIdName, FormatIdNameProps } from "./common/FormatIdName";
-import { FormEvent, useState } from "react";
+import { FormEvent, MouseEvent, useState } from "react";
 import {
   Alert,
   Card,
@@ -15,18 +15,23 @@ export type FollowingProps = {
   following: Set<string>;
   formatIdNameProps: Omit<FormatIdNameProps, "id">;
   followId: (id: string) => Promise<void>;
+  unfollowId: (id: string) => Promise<void>;
 };
 
 export function Following(props: FollowingProps) {
   const [idToFollow, setIdToFollow]: UseState<string> = useState("");
 
-  const follow = (event: FormEvent) => {
+  function follow(event: FormEvent) {
     event.preventDefault();
     props
       .followId(idToFollow)
       .then(() => setIdToFollow(""))
       .catch((x) => console.error(x));
-  };
+  }
+
+  function unfollow(event: MouseEvent) {
+    event.preventDefault();
+  }
 
   return (
     <Container className="my-3 max-width-md mx-auto">
@@ -56,7 +61,27 @@ export function Following(props: FollowingProps) {
           <ListGroup className="my-3">
             {[...props.following].map((x) => (
               <ListGroup.Item key={x}>
-                <FormatIdName id={x} {...props.formatIdNameProps} />
+                <div className="d-flex">
+                  <div className="me-auto">
+                    <FormatIdName id={x} {...props.formatIdNameProps} />
+                  </div>
+                  <div>
+                <small>
+                  <a
+                    href="#"
+                    onClick={(e: MouseEvent) => {
+                      e.preventDefault();
+                      props
+                        .unfollowId(x)
+                        .catch((err) => console.error(err));
+                    }}
+                    className="fw-normal bg-danger text-white rounded-pill mb-2 py-1 px-2"
+                  >
+                    Unfollow
+                  </a>
+                </small>
+                  </div>
+                </div>
               </ListGroup.Item>
             ))}
           </ListGroup>
