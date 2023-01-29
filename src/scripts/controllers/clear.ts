b94c5cbeb9376275575eeba2453b1d1bad1a9d77
Garrin McGoldrick <garrin.mcgoldrick@gmail.com>
2023-01-29 09:01:31 -0500

    fix issue deleting on non chrome

--- src/scripts/controllers/clear.ts
+++ src/scripts/controllers/clear.ts
@@ -1,14 +1,9 @@
 import { clearState } from "../commonutils";
+import { ChatterNet } from "chatternet-client-http";
 
 export async function clearAll() {
   sessionStorage.clear();
   localStorage.clear();
-  window.indexedDB
-    .databases()
-    .then((x) => {
-      for (const info of x)
-        if (info.name) window.indexedDB.deleteDatabase(info.name);
-    })
-    .catch((x) => console.error(x));
+  ChatterNet.clearDbs().catch((x) => console.error(x));
   clearState();
 }
